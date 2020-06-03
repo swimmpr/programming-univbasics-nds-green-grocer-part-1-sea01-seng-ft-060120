8d3f9c76110a0ec55d04bb0485ecce734ec6bc26
swimmpr <marshall.raiskin@gmail.com>
2020-06-03 14:28:51 -0700

    Done.

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,12 +1,6 @@
 require 'pry'
 
-# grocery_shelf = [
-#   { :item => "CANNED BEANS", :price => 3.00, :clearance => true },
-#   { :item => "CANNED CORN", :price => 2.50, :clearance => false },
-#   { :item => "SALSA", :price => 1.50, :clearance => false },
-#   { :item => "TORTILLAS", :price => 2.00, :clearance => false },
-#   { :item => "HOT SAUCE", :price => 1.75, :clearance => false }
-# ]
+
 
 def find_item_by_name_in_collection(name, collection)
   counter = 0
@@ -19,10 +13,7 @@ def find_item_by_name_in_collection(name, collection)
 end
 
 def consolidate_cart(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This returns a new Array that represents the cart. Don't merely
-  # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  
 new_cart = []
 counter = 0
 while counter < cart.length
